bedd868b1d07886854b4e62da0820b0856cc9ec3
Benedikt Maier <benedikt.maier@cern.ch>
2022-11-30 11:01:57 -0500

    buffering whole dataset into memory

--- deepjet_geometric/datasets/__init__.py
+++ deepjet_geometric/datasets/__init__.py
@@ -5,5 +5,7 @@ from .ticl_v2_dataset import TICLV2
 from .suep_v1_dataset import SUEPV1
 from .cl_v1_dataset import CLV1
 from .cl_v1root_dataset import CLV1ROOT
+from .cl_v2_dataset import CLV2
 
-__all__ = ['DeepJetCoreV1', 'DeepJetCoreV2', 'MetV1', 'TICLV1', 'TICLV2', 'SUEPV1', 'CLV1', 'CLV1ROOT']
+__all__ = ['DeepJetCoreV1', 'DeepJetCoreV2', 'MetV1', 'TICLV1', 'TICLV2', 'SUEPV1', 'CLV1', 'CLV1ROOT', 'CLV2']
+
